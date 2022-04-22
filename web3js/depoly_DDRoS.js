b481eec09e135e17ca7cde41a623e657dcfe5731
huge <1529827275@qq.com>
2022-04-22 16:30:56 +0800

    update the js

--- web3js/depoly_DDRoS.js
+++ web3js/depoly_DDRoS.js
@@ -1,27 +1,24 @@
-(async () =>{
-    try {
-        console.log('DDRoS defend server openning...')
+var Web3 = require('web3');
+var web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:7777'));
 
-        const contractName = 'xxx' // SpoofRecord contract or other contracts
-        const constructorArgs = [] // constructor args for contract
+web3.eth.getAccounts().then(console.log);
 
-        //ABI is generated from the compilation artifact.
-        const artifactsPath = 'Contracts/artifacts/${xxx}.json'
+console.log('Server is deploying...');
 
-        const metadata = JSON.parse(await remix.call('fileManger', 'getFile', artifactsPath))
+var markreportContract = new web3.eth.Contract([{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"controller","type":"address"},{"indexed":false,"internalType":"address","name":"reporter","type":"address"},{"indexed":false,"internalType":"string","name":"swTrace","type":"string"}],"name":"LogMarkInfo","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"controller","type":"address"},{"indexed":false,"internalType":"address","name":"reporter","type":"address"},{"indexed":false,"internalType":"string","name":"location","type":"string"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"},{"indexed":false,"internalType":"string","name":"_swTrace","type":"string"}],"name":"LogReport","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"controller","type":"address"},{"indexed":false,"internalType":"address","name":"reporter","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"LogTxInfo","type":"event"},{"inputs":[],"name":"getLocationList","outputs":[{"internalType":"string[]","name":"","type":"string[]"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getMarkInfo","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"getReporterList","outputs":[{"internalType":"address[]","name":"","type":"address[]"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getTxInfo","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address payable","name":"_controller","type":"address"},{"internalType":"string","name":"_location","type":"string"},{"internalType":"string","name":"_swTrace","type":"string"}],"name":"report","outputs":[],"stateMutability":"payable","type":"function"}]);
 
-        const signer = (new ethers.providers.Web3Provider(web3Provider)).getSigner()
+// data is compiled from bytecode
+markreportContract.options.data = 'xxx'; 
 
-        let factory = new ethers.ContractFactory(metadata.abi, metadata.data.bytecode.object, signer);
-
-        let contract = await factory.depoly(...constructorArgs);
-        
-        console.log('contract Address:', contract.address);
-
-        // The contract is NOT deployed yet; we must wait until it is mined
-        await contract.depolyed()
-        console.log('DDRoS server is working!')
-    } catch (e) {
-        console.log(e.message)
+var markreport = markreportContract.deploy({
+}).send({
+     from: '0xfdf15D3Fd7705D44Ec071a9d6C5B002aab989D74', // ethereum address to deploy the contract
+     gas: '4700000'
+   }, function (e, contract){
+    console.log(e, contract);
+    if (typeof contract.address !== 'undefined') {
+         console.log('Contract mined! address: ' + contract.address + ' transactionHash: ' + contract.transactionHash);
     }
-})()
+ }).then(function(contract){
+    console.log("Server is working! , Contract address is: ", contract.options.address);
+ }) 
